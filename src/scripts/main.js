6d0da5ebf324924ae6dd8a08de8859b25be869fc
Yurii Drutsul <druculurij@gmail.com>
2025-11-03 16:57:39 +0200

    message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,34 +1,35 @@
 'use strict';
 
-const listItems = [...document.querySelectorAll('ul li')];
+function parseSalary(salaryStr) {
+  return Number(salaryStr.split('$').join('').split(',').join(''));
+}
 
-function sortList(items) {
+const list = document.querySelector('ul');
+const listItems = [...list.querySelectorAll('li')];
+
+function sortList(items, listElement) {
   items.sort((a, b) => {
-    const salaryA = Number(
-      a.getAttribute('data-salary').split('$').join('').split(',').join(''),
-    );
-    const salaryB = Number(
-      b.getAttribute('data-salary').split('$').join('').split(',').join(''),
-    );
+    const salaryA = parseSalary(a.getAttribute('data-salary'));
+    const salaryB = parseSalary(b.getAttribute('data-salary'));
 
     return salaryB - salaryA;
   });
 
-  const list = document.querySelector('ul');
-
-  items.forEach((item) => list.appendChild(item));
+  items.forEach((item) => listElement.appendChild(item));
 }
 
 function getEmployees(items) {
-  return items.map((item) => ({
-    name: item.textContent.trim(),
-    position: item.getAttribute('data-position'),
-    salary: Number(
-      item.getAttribute('data-salary').split('$').join('').split(',').join(''),
-    ),
-    age: Number(item.getAttribute('data-age')),
-  }));
+  return items.map((item) => {
+    const salary = parseSalary(item.getAttribute('data-salary'));
+
+    return {
+      name: item.textContent.trim(),
+      position: item.getAttribute('data-position'),
+      salary,
+      age: Number(item.getAttribute('data-age')),
+    };
+  });
 }
 
-sortList(listItems);
+sortList(listItems, list);
 getEmployees(listItems);
