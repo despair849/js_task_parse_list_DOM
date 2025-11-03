c065896f1200ab1faca753b062b14a3ab0ffc17a
Yurii Drutsul <druculurij@gmail.com>
2025-11-03 16:38:42 +0200

    message

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,34 @@
 'use strict';
 
-// write code here
+const listItems = [...document.querySelectorAll('ul li')];
+
+function sortList(items) {
+  items.sort((a, b) => {
+    const salaryA = Number(
+      a.getAttribute('data-salary').split('$').join('').split(',').join(''),
+    );
+    const salaryB = Number(
+      b.getAttribute('data-salary').split('$').join('').split(',').join(''),
+    );
+
+    return salaryB - salaryA;
+  });
+
+  const list = document.querySelector('ul');
+
+  items.forEach((item) => list.appendChild(item));
+}
+
+function getEmployees(items) {
+  return items.map((item) => ({
+    name: item.textContent.trim(),
+    position: item.getAttribute('data-position'),
+    salary: Number(
+      item.getAttribute('data-salary').split('$').join('').split(',').join(''),
+    ),
+    age: Number(item.getAttribute('data-age')),
+  }));
+}
+
+sortList(listItems);
+getEmployees(listItems);
